1bd0b3af48d102b128edd39ee5170b54de320319
Vilis Ērglis <vilis.erglis@softdent.lt>
2022-02-01 21:14:48 +0200

    And here comes the rest

---
+++ BuildingManager.Repository/Repositories/Person/PersonQueries.cs
@@ -0,0 +1,25 @@
+﻿using BuildingManager.Repository.Constants;
+
+namespace BuildingManager.Repository.Repositories.Person;
+
+public static class PersonQueries
+{
+    public static string InsertPersonQuery()
+    {
+        return
+            $"INSERT INTO {DemographicsDb.Schema}.{DemographicsDb.Tables.Person} " +
+            $"(id, firstname, lastname, code, created_at)" +
+            $" VALUES (@Id, @Firstname, @Lastname, @PersonCode, @CreatedAt)";
+    }
+
+    public static string SelectPersonQQuery()
+    {
+        return $"SELECT " +
+               $"id AS @Id, " +
+               $"firstname AS @Firstname, " +
+               $"lastname AS @Lastname " +
+               $"code AS @PersonCode " +
+               $"created_at AS @CreatedAt " +
+               $"FROM {DemographicsDb.Schema}.{DemographicsDb.Tables.Person}";
+    }
+}
